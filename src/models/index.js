fe1317e115c1b22917f8beb90d6aaa6ed7bcef2c
uche09 <ucheomesu@gmail.com>
2025-11-01 19:21:38 +0100

    Defined `RefreshToken` model's relationship, and re-exported RefreshToken

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
 import User from "./user.js";
+import RefreshToken from "./refresh_token.js";
 
+// Relationships
+RefreshToken.belongsTo(User, {foreignKey: "userId"});
 
-
-export default {User};
+export default {User, RefreshToken};
