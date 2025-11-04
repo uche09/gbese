7a498dd8498aae7eb9e93ba0a4ab4e63bbc51ada
uche09 <ucheomesu@gmail.com>
2025-11-04 14:31:06 +0100

    Added Record table (Model) and defined it relationship

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,11 @@
 import User from "./user.js";
 import RefreshToken from "./refresh_token.js";
+import Record from "./record.js";
 
 // Relationships
+// User.hasMany(Record, {foreignKey: "userId"}),
+Record.belongsTo(User, {foreignKey: "userId"}),
 RefreshToken.belongsTo(User, {foreignKey: "userId"});
 
-export default {User, RefreshToken};
+
+export default {User, RefreshToken, Record};
