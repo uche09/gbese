e56bec6e57dc32a11cad7c8073d58f2f2104979c
uche09 <ucheomesu@gmail.com>
2025-11-05 13:00:10 +0100

    Created record validation file to organize validators and sanitizers for new records and subsequent record related request

---
+++ src/validators/record.js
@@ -0,0 +1,76 @@
+import { body } from "express-validator";
+
+const validateNewRec = [
+    body("customerName")
+        .trim()
+        .notEmpty()
+        .withMessage("Customer name is required")
+        .bail()
+        .isLength({max: 80})
+        .withMessage("Limit username to 50 characters")
+        .bail()
+        .toLowerCase(),
+        
+    body("customerPhoneNumber")
+        .optional()
+        .trim()
+        .isMobilePhone()
+        .withMessage("Invalid phone number"),
+        
+    body("email")
+        .optional()
+        .trim()
+        .isEmail()
+        .withMessage("Invalid Email"),
+    
+    body("amount")
+        .trim()
+        .notEmpty()
+        .toFloat()
+        .withMessage("Expected an amount"),
+
+    body("payment")
+        .optional()
+        .trim()
+        .toFloat()
+        .custom((value, {req}) => {
+            if (Number(value) > Number(req.body.amount)) {
+                throw new Error("Payment cannot outweigh purchased amount")
+            }
+            return true;
+        })
+        .withMessage("Payment cannot outweigh purchased amout"),
+
+    body("transactionType")
+        .trim()
+        .notEmpty()
+        .withMessage("Transaction type required")
+        .bail()
+        .toLowerCase()
+        .isIn(["borrow", "lend"])
+        .withMessage("Invalid transaction type"),
+    
+    body("dueDate")
+        .trim()
+        .notEmpty()
+        .withMessage("Due date required")
+        .bail()
+        .isDate()
+        .custom((value) => {
+            console.log(typeof value);
+            if (Date.parse(value) < Date.now()) {
+                throw new Error("Expected a future date");
+            }
+            return true;
+        }),
+
+    body("description")
+        .notEmpty()
+        .withMessage("Description type required")
+        .bail()
+        .isLength({max: 5_000})
+        .withMessage("Description too long"),
+    
+];
+
+export {validateNewRec};
